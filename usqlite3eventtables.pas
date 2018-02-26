b8ad879829a4cda9ad7d909f758699f2ba853a0c
Christian Ulrich <info@cu-tec.de>
2018-02-26 19:47:10 +0100

    Windows implementation to read Event Log

--- usqlite3eventtables.pas
+++ usqlite3eventtables.pas
@@ -5,8 +5,11 @@ unit uSqlite3EventTables;
 interface
 
 uses
-  Classes, SysUtils, uSqlite3Helper, sqlite3dyn, usqlite3virtualTable, ActiveX,
-  ComObj, Variants;
+  Classes, SysUtils, uSqlite3Helper, sqlite3dyn, usqlite3virtualTable
+  {$ifdef WINDOWS}
+    {$I windows_units.inc}
+  {$endif}
+  ;
 
 type
 
@@ -15,6 +18,9 @@ type
   TEventCursor = class(TSQLiteVirtualTableCursor)
   private
     FEof : Boolean;
+    {$ifdef WINDOWS}
+      {$I windows_cursorvars.inc}
+    {$endif}
   public
     function Search(Prepared : TSQLVirtualTablePrepared) : Boolean;override;
     function Column(Index : Integer;var Res : TSQLVar) : Boolean;override;
@@ -36,107 +42,9 @@ type
 
 implementation
 
-var
-  fModule: TSQLite3Module;
-
-{ TEventTable }
-
-function TEventTable.Prepare(var Prepared: TSQLVirtualTablePrepared): Boolean;
-var
-  i: Integer;
-  aPrep: PSQLVirtualTablePreparedConstraint;
-begin
-  for i := 0 to Prepared.WhereCount-1 do
-    begin
-      aPrep := @Prepared.Where[i];
-      aPrep^.Value.VType := ftNull;
-    end;
-  Result := True;
-end;
-
-function TEventTable.GetName: string;
-begin
-  Result := 'event';
-end;
-
-function TEventTable.CursorClass: TSQLiteVirtualTableCursorClass;
-begin
-  Result := TEventCursor;
-end;
-
-function TEventTable.GenerateStructure: string;
-const
-  Structure = 'create table events ('+
-  'name  text,'+
-  'path  text,'+
-  'isdir int,'+
-  'size  int,'+
-  'time  date'+
-  ')';
-begin
-  Result := Structure;
-end;
-
-{ TEventCursor }
-
-function TEventCursor.Search(Prepared: TSQLVirtualTablePrepared): Boolean;
-var
-  i: Integer;
-  aPrep: PSQLVirtualTablePreparedConstraint;
-  tmp: PUTF8Char;
-begin
-  Result := True;
-  for i := 0 to Prepared.WhereCount-1 do
-    begin
-    end;
-end;
-
-function TEventCursor.Column(Index: Integer; var Res: TSQLVar): Boolean;
-begin
-  Res.VType:=ftNull;
-{
-  case Index of
-  //-1:Res := Fsr.Time;
-  0:begin
-      Res.VType:=ftUTF8;
-      Res.VText:= PUTF8Char(FSearchRecs[length(FSearchRecs)-1].Name);//name
-    end;
-  1:begin
-      Res.VType:=ftUTF8;
-      Res.VText:=PUTF8Char(FPath);//path
-    end;
-  2:begin
-      Res.VType:=ftInt64;
-      if FSearchRecs[length(FSearchRecs)-1].Attr and faDirectory = faDirectory then
-        Res.VInt64:= 1
-      else
-        Res.VInt64:= 0; //isdir
-    end;
-  3:begin
-      Res.VType:=ftInt64;
-      Res.VInt64 := FSearchRecs[length(FSearchRecs)-1].Size;//size
-    end;
-  4:begin
-      Res.VType:=ftDate;
-      Res.VDateTime:=FileDateToDateTime(FSearchRecs[length(FSearchRecs)-1].Time); //mtime
-    end;
-  //ctime
-  //atime
-  end;
-}
-  Result := True;
-end;
-
-function TEventCursor.Next: Boolean;
-label retry;
-begin
-  Result := True;
-end;
-
-function TEventCursor.Eof: Boolean;
-begin
-  result := FEof;
-end;
+{$ifdef WINDOWS}
+  {$I windows_implementation.inc}
+{$endif}
 
 end.
 
